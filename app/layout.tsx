b70438c01fddda3ece9df5e6fd54a4bf33ff39aa
S-al1 <salmaaitabbou972@gmail.comi>
2025-09-14 01:05:41 +0100

    final commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Azeddine Harchaoui Portfolio',
+  description: 'This is the portfolio of Azeddine Harchaoui',
 }
 
 export default function RootLayout({
